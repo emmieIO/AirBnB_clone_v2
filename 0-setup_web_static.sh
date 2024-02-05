9f87cb21b1eb1cd9dad6ae1203e317d2d0e298b0
emmieIO <markonuoha6@gmail.com>
2024-02-05 15:50:58 +0100

    AirBNB_clone_v2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,38 +1,52 @@
-#!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+#!usr/bin/env bash
 
-apt-get update
-apt-get install -y nginx
+# Install Nginx if not already installed
+if ! command -v nginx &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get install -y nginx
+fi
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Create necessary folders
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+# Create a fake HTML file
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
-printf %s "server {
+# Create or recreate symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership of /data/ folder to ubuntu user and group recursively
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update Nginx configuration
+config_content="server {
     listen 80 default_server;
     listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
+
+    root /var/www/html;
+    index index.html index.htm;
+
+    server_name _;
 
     location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
+        alias /data/web_static/current/;
+        index index.html;
     }
 
-    location /redirect_me {
-        return 301 http://cuberule.com/;
+    location / {
+        try_files \$uri \$uri/ =404;
     }
+}"
 
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
+echo "$config_content" | sudo tee /etc/nginx/sites-available/default > /dev/null
 
-service nginx restart
+# Restart Nginx
+sudo service nginx restart
+
+exit 0
